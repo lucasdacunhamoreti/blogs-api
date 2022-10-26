2966ea2dc4d697727420531505e65fb27a81ec37
Lucas da Cunha Moreti <lucasdacunha00@gmail.com>
2022-10-25 21:19:24 -0300

    fix: error lint on userSchemas

--- src/validations/schemas.js
+++ src/validations/schemas.js
@@ -1,38 +1,32 @@
-// const yup = require('yup');
 const Joi = require('joi');
 
-// const EMPTY_FIELD = 'Some required fields are missing';
-
-// const schemaUser = yup.object({
-//     email: yup.string().email().required(EMPTY_FIELD),
-//     password: yup.string().required(EMPTY_FIELD),
-// });
+const MISSING_FIELD = 'Some required fields are missing';
 
 const schemaUser = Joi.object({
     email: Joi.string().email().required().messages({
-        'any.required': 'Some required fields are missing',
-        'string.empty': 'Some required fields are missing',
+        'any.required': MISSING_FIELD,
+        'string.empty': MISSING_FIELD,
     }),
     password: Joi.string().required().messages({
-        'any.required': 'Some required fields are missing',
-        'string.empty': 'Some required fields are missing',
+        'any.required': MISSING_FIELD,
+        'string.empty': MISSING_FIELD,
     }),
 });
 
 const schemaNewUser = Joi.object({
     displayName: Joi.string().required().min(8)
     .messages({
-        'any.required': 'Some required fields are missing',
-        'string.empty': 'Some required fields are missing',
+        'any.required': MISSING_FIELD,
+        'string.empty': MISSING_FIELD,
         'string.min': '"displayName" length must be at least 8 characters long',
     }),
     email: Joi.string().email().required().messages({
-        'any.required': 'Some required fields are missing',
-        'string.empty': 'Some required fields are missing',
+        'any.required': MISSING_FIELD,
+        'string.empty': MISSING_FIELD,
     }),
     password: Joi.string().min(6).required().messages({
-        'any.required': 'Some required fields are missing',
-        'string.empty': 'Some required fields are missing',
+        'any.required': MISSING_FIELD,
+        'string.empty': MISSING_FIELD,
         'string.min': '"password" length must be at least 6 characters long',
     }),
     image: Joi.string(),
