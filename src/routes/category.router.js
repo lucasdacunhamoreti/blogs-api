1983f5356691b557c2bcdd5db93f0d7f87c0957b
Lucas da Cunha Moreti <lucasdacunha00@gmail.com>
2022-10-25 22:13:31 -0300

    feat: Create endpoint get /categories

--- src/routes/category.router.js
+++ src/routes/category.router.js
@@ -5,5 +5,6 @@ const authMiddleware = require('../middlewares/auth.middleware');
 const router = express.Router();
 
 router.post('/', authMiddleware.validateToken, categoryController.newCategory);
+router.get('/', authMiddleware.validateToken, categoryController.getCategories);
 
 module.exports = router;
